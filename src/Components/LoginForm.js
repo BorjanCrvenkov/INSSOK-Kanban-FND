b2a7e02f30f97c2be50c5e7c8055a96c65e966d7
Gakovski <david_gakovski@outlook.com>
2023-02-28 14:01:12 +0100

    login form in bootstrap

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -2,67 +2,57 @@ import React from 'react';
 import BaseRepository from "./Repository/BaseRepository";
 
 class LoginForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: '',
-            repository: new BaseRepository(),
-        };
-        this.onInputchange = this.onInputchange.bind(this);
-        this.onSubmitForm = this.onSubmitForm.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      repository: new BaseRepository(),
+    };
+    this.onInputchange = this.onInputchange.bind(this);
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+  };
+
+  onInputchange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  async onSubmitForm(e) {
+    e.preventDefault()
+
+    const data = {
+      'email': this.state.email,
+      'password': this.state.password,
     };
 
-    onInputchange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
+    await this.state.repository.login(data);
 
-    async onSubmitForm(e) {
-        e.preventDefault()
-
-        const data = {
-            'email': this.state.email,
-            'password': this.state.password,
-        };
-
-        await this.state.repository.login(data);
-
-        window.location.href = 'http://localhost:3000/workspaces'
-    }
+    window.location.href = 'http://localhost:3000/workspaces'
+  }
 
 
-    render() {
-        return (
-          <div className="login-form-container">
-            <form className="login-form" onSubmit={this.onSubmitForm}>
-              <label className="login-form-label">Email</label>
-              <input
-                className="login-form-input"
-                type="email"
-                name="email"
-                value={this.state.email}
-                onChange={this.onInputchange}
-                required
-              />
-              <label className="login-form-label">Password</label>
-              <input
-                className="login-form-input"
-                type="password"
-                name="password"
-                value={this.state.password}
-                onChange={this.onInputchange}
-                required
-              />
-              <button className="login-form-button" type="submit">
-                Submit
-              </button>
-            </form>
+  render() {
+    return (
+      <div class="w-25 mx-auto">
+        <form>
+          <div class="form-outline mt-4">
+            <input type="email" id="id_email" class="form-control" />
+            <label class="form-label" for="id_email">Email address</label>
           </div>
-        );
-      }
-      
+
+          <div class="form-outline mt-4">
+            <input type="password" id="id_password" class="form-control" />
+            <label class="form-label" for="id_password">Password</label>
+          </div>
+
+          <button type="button" class="btn btn-primary btn-block mt-4">Sign in</button>
+        </form>
+      </div>
+    );
+  }
+
 }
 
 export default LoginForm;
