b1397fe35780b9bb7065ea0debf2f472c1d0ca7b
Gakovski <david_gakovski@outlook.com>
2023-04-18 22:40:48 +0200

    added bootstrap classes to TaskDisplayModal.js

--- src/Components/Task/TaskDisplayModal.js
+++ src/Components/Task/TaskDisplayModal.js
@@ -20,53 +20,80 @@ class TaskDisplayModal extends React.Component {
     };
 
     render() {
-        let {showModal, task} = this.state;
+        let { showModal, task } = this.state;
         return (
-            <>
-                <Modal show={showModal} onHide={this.toggleModal}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>{task.label}</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body><div>
-                        <label><b>Task title:</b></label>
-                        <p>{task.title}</p>
-                        <label><b>Task description:</b></label>
-                        <p>{task.description}</p>
-                        <label><b>Task priority:</b></label>
-                        <p>{task.priority}</p>
-                        <label><b>Task due_date:</b></label>
-                        <p>{task.due_date}</p>
-                        <label><b>Task type:</b></label>
-                        <p>{task.type}</p>
-                        <label><b>Task label:</b></label>
-                        <p>{task.label}</p>
-                        <label><b>Task assignee:</b></label>
-                        {/*moze i slika za ovie dvajca?*/}
-                        <p>{task.assignee.first_name} {task.assignee.last_name}</p>
-                        <label><b>Task reporter:</b></label>
-                        <p>{task.reporter.first_name} {task.reporter.last_name}</p>
-                        <label><b>Task comments:</b></label>
-                        <h1></h1>
-                        {task.comments.map(function (comment, key) {
-                            return <>
-                                <label><b>Comment body:</b></label>
-                                <p>{comment.body}</p>
-                                {/*moze i slika za ovoj?*/}
-                                <p>Posted by: {comment.user.first_name} {comment.user.last_name}</p>
-                                <p>Posted at: {comment.created_at}</p>
-                            </>
-                        })}
-
-                    </div></Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={this.toggleModal}>
-                            Close
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-            </>
+          <>
+            <Modal show={showModal} onHide={this.toggleModal} size="xl">
+              <Modal.Header closeButton>
+                <Modal.Title className="font-weight-bold text-left">
+                  <h2 className="font-weight-bold">{task.title}</h2>
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div className="row">
+                  <div className="col-md-8">
+                    <h4 className="font-weight-bold">Description</h4>
+                    <p className="lead">{task.description}</p>
+                    <h5 className="mt-4 mb-3">Comments</h5>
+                    <div className="border rounded p-3">
+                      {task.comments.map(function (comment, key) {
+                        return (
+                          <div className="border-bottom pb-3 mb-3" key={key}>
+                            <p>{comment.body}</p>
+                            <p className="text-muted">
+                              Posted by: {comment.user.first_name} {comment.user.last_name}
+                              <br />
+                              Posted at: {comment.created_at}
+                            </p>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <div className="mb-3">
+                      <label className="font-weight-bold">Priority:</label>
+                      <p>{task.priority}</p>
+                    </div>
+                    <div className="mb-3">
+                      <label className="font-weight-bold">Due Date:</label>
+                      <p>{task.due_date}</p>
+                    </div>
+                    <div className="mb-3">
+                      <label className="font-weight-bold">Type:</label>
+                      <p>{task.type}</p>
+                    </div>
+                    <div className="mb-3">
+                      <label className="font-weight-bold">Label:</label>
+                      <p>{task.label}</p>
+                    </div>
+                    <div className="mb-3">
+                      <label className="font-weight-bold">Assignee:</label>
+                      <p>
+                        {task.assignee.first_name} {task.assignee.last_name}
+                      </p>
+                    </div>
+                    <div className="mb-3">
+                      <label className="font-weight-bold">Reporter:</label>
+                      <p>
+                        {task.reporter.first_name} {task.reporter.last_name}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={this.toggleModal}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </>
         );
-    }
+      }
+      
+      
+    
 }
 
 export default TaskDisplayModal;
